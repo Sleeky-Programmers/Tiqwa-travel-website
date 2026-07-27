59a1644c54089553a10021ed8e91778e92821654
Shiloh George <shinohgeorge2019@gmail.com>
2026-07-27 08:26:14 +0100

    feat: commented out static details on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,37 @@
-import { HeroSection } from "@/components/features/home/HeroSection";
-import { TrustBanner } from "@/components/features/home/TrustBanner";
-import { PopularRoutes } from "@/components/features/home/PopularRoutes";
-import { HolidayPackages } from "@/components/features/home/HolidayPackages";
-import { WhyChooseUs } from "@/components/features/home/WhyChooseUs";
-import { DreamDestinations } from "@/components/features/home/DreamDestinations";
-import { TestimonialsSection } from "@/components/features/home/TestimonialsSection";
-import { CtaBand } from "@/components/features/home/CtaBand";
-import {
-  getHomepageData,
-  getPopularAirports,
-} from "@/services/whitelabel-api";
-import { PublicLayout } from "@/components/layout/PublicLayout";
+import { CtaBand } from '@/components/features/home/CtaBand';
+import { DreamDestinations } from '@/components/features/home/DreamDestinations';
+import { HeroSection } from '@/components/features/home/HeroSection';
+import { HolidayPackages } from '@/components/features/home/HolidayPackages';
+import { PopularRoutes } from '@/components/features/home/PopularRoutes';
+// import { TestimonialsSection } from '@/components/features/home/TestimonialsSection';
+// import { TrustBanner } from '@/components/features/home/TrustBanner';
+// import { WhyChooseUs } from '@/components/features/home/WhyChooseUs';
+import { PublicLayout } from '@/components/layout/PublicLayout';
+import { getHomepageData, getPopularAirports } from '@/services/whitelabel-api';
 
 export default async function HomePage() {
-  const [homepage, popularAirports] = await Promise.all([
-    getHomepageData().catch(() => ({
-      hero_banners: [],
-      holiday_packages: [],
-    })),
-    getPopularAirports().catch(() => []),
-  ]);
+	const [homepage, popularAirports] = await Promise.all([
+		getHomepageData().catch(() => ({
+			hero_banners: [],
+			holiday_packages: [],
+		})),
+		getPopularAirports().catch(() => []),
+	]);
 
-  const heroImage = homepage.hero_banners?.[0]?.image;
+	const heroImage = homepage.hero_banners?.[0]?.image;
 
-  return (
-    <PublicLayout>
-    <div className="page-fade-in">
-      <HeroSection heroImage={heroImage} />
-      <TrustBanner />
-      <PopularRoutes />
-      <HolidayPackages packages={homepage.holiday_packages ?? []} />
-      <WhyChooseUs />
-      <DreamDestinations popularAirports={popularAirports} />
-      <TestimonialsSection />
-      <CtaBand />
-    </div>
-    </PublicLayout>
-  );
+	return (
+		<PublicLayout>
+			<div className="page-fade-in">
+				<HeroSection heroImage={heroImage} />
+				{/* <TrustBanner /> */}
+				<PopularRoutes />
+				<HolidayPackages packages={homepage.holiday_packages ?? []} />
+				{/* <WhyChooseUs /> */}
+				<DreamDestinations popularAirports={popularAirports} />
+				{/* <TestimonialsSection /> */}
+				<CtaBand />
+			</div>
+		</PublicLayout>
+	);
 }
